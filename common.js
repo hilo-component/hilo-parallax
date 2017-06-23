611268d88150d806b628fb0d490037047e75dfeb
jljsj <liangjun.jiang@alipay.com>
2017-06-23 18:53:04 +0800

    Updates

--- common.js
+++ common.js
@@ -1643,7 +1643,7 @@ var initStage = function () {
     this.dpr = window.devicePixelRatio;
 
     this.onResizeWindowElement = function () {
-      var names = ['body > h1', '#qrcode', 'body > .highlight', 'body > .header'];
+      var names = ['body > h1', '#qrcode', 'body > .highlight', 'body > .header', '.container .header', '.container > .highlight'];
       if (window.innerWidth < 768) {
         _this.stageMobileShow(names, 'none');
       } else {
@@ -1663,7 +1663,7 @@ var initStage = function () {
       var container = document.querySelector('.container');
       if (container) {
         if (s === 'none') {
-          container.style.cssText = 'width: 100%; max-width: auto;';
+          container.style.cssText = 'width: 100%; max-width: 100%;';
           root.style.padding = '0';
           document.body.style.margin = '0';
         } else {
